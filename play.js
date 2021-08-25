3bea675263e2c6ca484a1f2722abaa30f244cc94
SJ <sj.jega2@gmail.com>
2021-08-25 17:34:04 +0000

    commit after 4th acitivity completion

--- play.js
+++ play.js
@@ -5,3 +5,32 @@ console.log("Connecting ...");
 connect();
 
 
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput(data));
+  return stdin;
+};
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    console.log("User input terminated")
+    process.exit();
+  }
+  console.log(data)
+}
+
+
+// stdin.on('data', (key) => {
+//   if (key === '\u0003') {
+//     console.log("Terminate key pressed");
+//     process.exit();
+//   }  
+//   process.stdout.write('.');
+// });
