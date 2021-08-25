b3191877be70d2afab0c020cd6235f095d8840e0
SJ <sj.jega2@gmail.com>
2021-08-25 17:07:07 +0000

    commit after 1st activity completion

--- play.js
+++ play.js
@@ -3,15 +3,26 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const serverConnectionObject = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541
+    host: "135.23.223.133",
+    port: 50542
   });
 
   // interpret incoming data as text
   serverConnectionObject.setEncoding("utf8");
+
+  serverConnectionObject.on('connect', () => {
+    console.log("Connection with server established");
+  })
+
+  serverConnectionObject.on('data', (data) => {
+    console.log(data);
+  })  
+  
 
   return serverConnectionObject;
 };
 
 console.log("Connecting ...");
 connect();
+
+
